baef16e9b9cb9196884eb0ec7f8c54dc817b8550
Naveen Kairamkonda <naveenkairamkonda26@gmail.com>
2024-01-07 12:16:57 +0530

    config: dev and prod webpack builds and ts declarations for images and svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './App.css';
+import reactPng from './react-png.png';
+import reactSvg from './react-svg.svg';
 
 const App = () => {
 
   return (
-    <div>React App with Webpack template</div>
+    <div>
+      <h1>React App with Webpack template -{process.env.NODE_ENV} {process.env.name}
+       </h1>
+      <img src={reactPng} alt="react-logo-png" width="200px" height="200px" />
+      <img src={reactSvg} alt="react-logo-svg" width="200px" height="200px" />
+    </div>
   )
 }
 
---
+++ webpack/webpack.config.js
@@ -0,0 +1,11 @@
+/** merge both webpack common & dev/prod mode */
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common.js')
+
+module.exports = (envVars) => {
+    const { env } = envVars;
+    
+    const envConfig = require(`./webpack.${env}.js`)
+    const config = merge(commonConfig, envConfig)
+    return config
+}
